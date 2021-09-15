2e9800bde73a8b5b70f5b3cba0b0c502d21afd1a
MARS66 <1908367780@qq.com>
2021-09-15 16:10:31 +0800

    接口调试

--- luying/pages/product/product.js
+++ luying/pages/product/product.js
@@ -9,16 +9,18 @@ Page({
     products:[],       
     tabs:['最新','最热'],
     currentIndex:0,
+    num:10,
+    page:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
- async getdata(order=0) {
+ async getdata() {
   const data= await getProduct({
-    order,
-    num:10,
-    page:1,
+    order:this.data.currentIndex,
+    num:this.data.num,
+    page:this.data.page,
   });
   this.setData({
     products:data.list,
@@ -32,14 +34,29 @@ Page({
     this.setData({
       currentIndex:index,
     })
-    this.getdata(index);
+    this.getdata();
   },
  
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  async onReachBottom () {
+  const data= await getProduct({
+    order:this.data.currentIndex,
+    num:this.data.num,
+    page:this.data.num+1,
+  });
+  this.setData({
+    num:this.data.num,
+    page:this.data.num+1,
+    products:this.data.products.push(...data),
+  })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
@@ -70,12 +87,7 @@ Page({
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
+  
 
   /**
    * 用户点击右上角分享
